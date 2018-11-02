fcb7303b79676c7ce4c0cf5ccdf4c681d7698f74
vstarush <vstarush@gmail.com>
2018-11-01 20:10:17 -0400

    Delete add-task.directive.spec.ts

--- directives/add-task.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AddTaskGroupDirective } from './add-task.directive';
-
-describe('AddTaskDirective', () => {
-  it('should create an instance', () => {
-    const directive = AddTaskGroupDirective;
-    expect(directive).toBeTruthy();
-  });
-});
